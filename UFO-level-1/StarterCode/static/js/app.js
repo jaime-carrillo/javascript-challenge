cd42875898ed707c1db2ecd400fa12206064953c
jaime-carrillo <66050851+jaime-carrillo@users.noreply.github.com>
2020-10-19 15:45:10 -0700

    push

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// console.log(data);
+
+// Values for each sighting and append one row for each sighting
+data.forEach(function(UfoSighting) {
+    var row = tbody.append("tr");
+
+
+    Object.entries(UfoSighting).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select
