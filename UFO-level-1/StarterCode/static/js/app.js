3974a7b237d8666950ffd347a107b8a96c90472c
jaime-carrillo <66050851+jaime-carrillo@users.noreply.github.com>
2020-10-19 19:10:47 -0700

    ufo sightings

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -7,14 +7,42 @@ var tbody = d3.select("tbody");
 // console.log(data);
 
 // Values for each sighting and append one row for each sighting
-data.forEach(function(UfoSighting) {
+tableData.forEach(function(ufoSighting) {
     var row = tbody.append("tr");
 
 
-    Object.entries(UfoSighting).forEach(function([key, value]) {
+    Object.entries(ufoSighting).forEach(function([key, value]) {
         var cell = row.append("td");
         cell.text(value);
     });
 });
 
-var button = d3.select
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    tbody.html("");
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    // console.log(inputValue)
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    // console.log(filteredData);
+
+    filteredData.forEach(function(selections) {
+        // console.log(selections);
+
+        var row = tbody.append("tr");
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
+
+    });
+
+});
